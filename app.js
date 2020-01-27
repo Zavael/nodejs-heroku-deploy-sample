bfc16f396857c83ec94964af03a288231ff54503
Zavael <cccp14@atlas.sk>
2020-01-27 13:49:13 +0100

    basic express server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var app = express();
+const port = process.env.PORT || 3000;
+ 
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+ 
+app.listen(port, () => console.log(`Listening on port ${port}`));
